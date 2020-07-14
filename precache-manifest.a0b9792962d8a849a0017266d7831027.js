6fd5727c483ea2959a9d8e39617ebeef61bd2a45
Anas Ahmed <anasahmed700@gmail.com>
2020-07-14 22:00:52 +0500

    Updates

---
+++ precache-manifest.a0b9792962d8a849a0017266d7831027.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "550585e0e9a22bc3a4cc9fd75ed55505",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "344a1f133f43e3f22a50",
+    "url": "/monsters-rolodex/static/css/main.ee1865a5.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "344a1f133f43e3f22a50",
+    "url": "/monsters-rolodex/static/js/main.aae6879e.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
